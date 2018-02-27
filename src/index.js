f5f291f92e8c1916ff86392a035c0287eaeed20b
donkeypagoda <christopherallenstewart@gmail.com>
2018-02-27 15:19:20 -0800

    creating components

--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Grid extends React.Component{
+  render(){
+    return(
+      <div>
+      Grid
+      </div>
+    )
+  }
+}
+
 class Main extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      generation: 0,
+    }
+  }
   render(){
     return (
       <div>
-      
+        <h1>Game of Life</h1>
+        <Grid
+        />
+        <h2>Generations: {this.state.generation}</h2>
       </div>
     )
   }
