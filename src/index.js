e50d3f1954c48f9bb9bda697f60441a9a84d35c7
donkeypagoda <christopherallenstewart@gmail.com>
2018-02-27 17:16:56 -0800

    working on control buttons

--- src/index.js
+++ src/index.js
@@ -50,6 +50,14 @@ class Grid extends React.Component{
   }
 }
 
+class Buttons extends React.Component{
+  render(){
+    return(
+      
+    )
+  }
+}
+
 class Main extends React.Component{
   constructor(){
     super()
@@ -84,14 +92,58 @@ class Main extends React.Component{
     })
   }
 
+  playButton = () =>{
+    clearInterval(this.intervalId)
+    this.intervalId = setInterval(this.play, this.speed)
+  }
+
+  pauseButton = () => {
+    clearInterval(this.intervalId)
+  }
+
+  play = () => {
+    let g = this.state.gridFull
+    let g2 = arrayClone(this.state.gridFull)
+
+    for (let i = 0; i < this.rows; i++) {
+		  for (let j = 0; j < this.cols; j++) {
+		    let count = 0;
+		    if (i > 0) if (g[i - 1][j]) count++;
+		    if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+		    if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+		    if (j < this.cols - 1) if (g[i][j + 1]) count++;
+		    if (j > 0) if (g[i][j - 1]) count++;
+		    if (i < this.rows - 1) if (g[i + 1][j]) count++;
+		    if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+		    if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+		    if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+		    if (!g[i][j] && count === 3) g2[i][j] = true;
+		  }
+		}
+		this.setState({
+		  gridFull: g2,
+		  generation: this.state.generation + 1
+    });
+  }
+
   componentDidMount(){
     this.seed()
+    this.playButton()
   }
 
   render(){
     return (
       <div>
         <h1>Game of Life</h1>
+        <Buttons
+          playButton={this.playButton}
+          pauseButton={this.pauseButton}
+          slow={this.slow}
+          fast={this.fast}
+          clear={this.clear}
+          seed={this.seed}
+          gridSize={this.gridSize}
+        />
         <Grid
           gridFull={this.state.gridFull}
           rows={this.rows}
